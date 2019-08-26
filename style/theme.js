1e16043c76cc8fa029d009b53d3547bb82f35775
KuwabataK <monorod98@gmail.com>
2019-08-27 00:59:06 +0900

    テーマカラーを変更

--- style/theme.js
+++ style/theme.js
@@ -6,7 +6,7 @@ import { red } from '@material-ui/core/colors';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#e64a19',
     },
     secondary: {
       main: '#19857b',
