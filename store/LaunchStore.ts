4164a66f03be24f79d86b8f906d5dc60de545d8b
KuwabataK <monorod98@gmail.com>
2019-08-28 00:13:46 +0900

    namespaceを追加

--- store/LaunchStore.ts
+++ store/LaunchStore.ts
@@ -2,19 +2,21 @@ import { observable } from "mobx";
 import moment from "moment";
 
 export class RestaurantStore {
-    @observable restaurants: {[key:string]:Restaurant} = {}
+    @observable restaurants: { [key: string]: LaunchStoreClasses.Restaurant } = {}
 }
 
-class Restaurant{
-    id: string = ''
-    title: string = ''
-    description: string = ''
-    coordinate: string = ''
-    holidays: Days[] = []
-    businessHours = {
-        from: moment("10:00:00", 'HH:mm:ss'),
-        to: moment("22:00:00", 'HH:mm:ss')
+export namespace LaunchStoreClasses {
+    export class Restaurant {
+        id: string = ''
+        title: string = ''
+        description: string = ''
+        location: string = ''
+        holidays: Days[] = []
+        businessHours = {
+            from: moment("10:00:00", 'HH:mm:ss'),
+            to: moment("22:00:00", 'HH:mm:ss')
+        }
     }
-}
 
-type Days = 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday' | 'sunday'
+    export type Days = 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday' | 'sunday'
+}
